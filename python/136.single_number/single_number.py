914087f393eda8f1f1a8473828d8c3cfccd491e2
grwkremilek <grwkremilek@users.noreply.github.com>
2019-04-10 21:08:21 +0300

    Delete single_number.py

--- python/136.single_number/single_number.py
+++
@@ -1,8 +0,0 @@
-#https://leetcode.com/problems/single-number/
-
-def singleNumber(nums):
-    s_nums = sorted(nums)
-    for i,j in zip(s_nums[0::2], s_nums[1::2]):
-        if i != j:
-            return i
-    return s_nums[-1]
