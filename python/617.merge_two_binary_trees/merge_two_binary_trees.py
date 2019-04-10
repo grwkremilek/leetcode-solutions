730e0322273a566066b17039d2694362b2943eb5
grwkremilek <grwkremilek@users.noreply.github.com>
2019-04-10 21:09:08 +0300

    Delete merge_two_binary_trees.py

--- python/617.merge_two_binary_trees/merge_two_binary_trees.py
+++
@@ -1,8 +0,0 @@
-#https://leetcode.com/problems/merge-two-binary-trees/
-
-def mergeTrees(t1, t2):
-    if t1 or t2:
-        root = TreeNode((t1 and t1.val or 0) + (t2 and t2.val or 0))
-        root.left = self.mergeTrees(t1 and t1.left, t2 and t2.left)
-        root.right = self.mergeTrees(t1 and t1.right, t2 and t2.right)
-        return root
