16e1e8dd3dbceb394a2d15618a0679f3037fdcd1
grw.kremilek <zuzana.krenkova@gmail.com>
2019-04-12 19:22:29 +0300

    matrix transposal, list(map(zip(*)))

---
+++ python/463.island_perimeter/island_perimeter.py
@@ -0,0 +1,9 @@
+#https://leetcode.com/problems/island-perimeter/
+
+
+def islandPerimeter(grid):
+    area = 0
+    for row in grid + list(map(list, zip(*grid))):
+        for i1, i2 in zip([0] + row, row + [0]):
+            area += int(i1 != i2)
+    return area
