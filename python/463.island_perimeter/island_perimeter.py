d1e099eae1647a37aafe5300179e5f2caf0f2e5a
grwkremilek <grwkremilek@users.noreply.github.com>
2019-04-27 09:47:25 +0300

    Delete island_perimeter.py

--- python/463.island_perimeter/island_perimeter.py
+++
@@ -1,9 +0,0 @@
-#https://leetcode.com/problems/island-perimeter/
-
-
-def islandPerimeter(grid):
-    area = 0
-    for row in grid + list(map(list, zip(*grid))):
-        for i1, i2 in zip([0] + row, row + [0]):
-            area += int(i1 != i2)
-    return area
