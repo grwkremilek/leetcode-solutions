81b760dc5eef404d1338fd8c8deb4a21457994a2
grwkremilek <grwkremilek@users.noreply.github.com>
2019-04-29 21:06:08 +0300

    Delete reshape_the_matrix.py

--- python/566.reshape-the-matrix/reshape_the_matrix.py
+++
@@ -1,8 +0,0 @@
-#https://leetcode.com/problems/reshape-the-matrix/
-
-def matrixReshape(nums, r, c):
-    if len(nums) * len(nums[0]) != r*c:
-        return nums
-    flat = [item for sublist in nums for item in sublist]
-    return [flat[x:x+c] for x in range(0, len(flat), c)]
-	
