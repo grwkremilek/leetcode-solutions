88b0e5a610b4d034d557b3b04e225289ed7f06f7
grwkremilek <grwkremilek@users.noreply.github.com>
2019-05-01 21:00:24 +0300

    Delete find_the_difference.py

--- python/0389.find_the_difference/find_the_difference.py
+++
@@ -1,8 +0,0 @@
-#https://leetcode.com/problems/find-the-difference/
-
-def findTheDifference(s, t):
-    s = sorted(s); t = sorted(t)
-    for index, (i,j) in enumerate(zip(s,t)):
-        if i != j:
-            return j if t[index+1] == i else i
-    return t[-1]
