551820c79179949d3cabe015fb6d53dddadf30ea
grwkremilek <grwkremilek@users.noreply.github.com>
2019-05-01 20:58:42 +0300

    Delete two_city_scheduling.py

--- python/1029.two_city_scheduling/two_city_scheduling.py
+++
@@ -1,5 +0,0 @@
-#https://leetcode.com/problems/two-city-scheduling/
-
-def twoCitySchedCost(costs):
-    costs = sorted(costs, key=lambda cost: cost[0]-cost[1])
-    return sum(cost[0] for cost in costs[:len(costs)//2]) + sum(cost[1] for cost in costs[len(costs)//2:])
