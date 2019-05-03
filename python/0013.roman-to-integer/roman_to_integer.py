65be122ee73cee999a3e2c2202dd501e30ab5acc
grw.kremilek <zuzana.krenkova@gmail.com>
2019-05-03 20:27:19 +0300

    dictionary

---
+++ python/0013.roman-to-integer/roman_to_integer.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/roman-to-integer/
+
+def romanToInt(s):
+    roman = {'M': 1000,'D': 500 ,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}
+    out = 0
+    for i in range(0, len(s) - 1):
+        if roman[s[i]] < roman[s[i+1]]:
+            out -= roman[s[i]]
+        else:
+            out += roman[s[i]]
+    return out + roman[s[-1]]
