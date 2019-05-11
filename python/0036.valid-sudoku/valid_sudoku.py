75679e3b68949b5b82434e0e521fe2427146e00e
grw.kremilek <zuzana.krenkova@gmail.com>
2019-05-11 15:19:02 +0300

    tuples

--- python/0036.valid-sudoku/valid_sudoku.py
+++ python/0036.valid-sudoku/valid_sudoku.py
@@ -1,6 +1,6 @@
 #https://leetcode.com/problems/valid-sudoku/
 
-def isValidSudoku(self, board: List[List[str]]) -> bool:
+def isValidSudoku(board):
     seen = sum(([(c, i), (j, c), (int(i/3), int(j/3), c)]
                 for i, row in enumerate(board)
                 for j, c in enumerate(row)
