2f81dbecaf9d30757f2404e5a8aaad5237bb5a57
grw.kremilek <zuzana.krenkova@gmail.com>
2019-06-20 21:43:00 +0300

    dictionary, git add *!

---
+++ python/0003.longest-substring-without-repeating-characters/README.md
@@ -0,0 +1,6 @@
+# Longest Substring Without Repeating Characters
+
+Given a string, find the length of the longest substring without repeating characters.
+
+#### Runtime: 56 ms, faster than 96.75% 
+
---
+++ python/0003.longest-substring-without-repeating-characters/longest_substring_without_repeating_characters.py
@@ -0,0 +1,12 @@
+#https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+def lengthOfLongestSubstring(s):
+    used = {}
+    max_length = start = 0
+    for i, c in enumerate(s):
+        if c in used and start <= used[c]:
+            start = used[c] + 1
+        else:
+            max_length = max(max_length, i - start + 1)   
+        used[c] = i
+    return max_length
