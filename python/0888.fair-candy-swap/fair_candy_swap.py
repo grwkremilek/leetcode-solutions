73efab20c3eb98e23078239a3c13f9deb58a006a
grw.kremilek <zuzana.krenkova@gmail.com>
2019-06-28 21:51:10 +0300

    list(map(lambda())), list(set() & set())

--- python/0888.fair-candy-swap/fair_candy_swap.py
+++ python/0888.fair-candy-swap/fair_candy_swap.py
@@ -1,7 +1,7 @@
 #https://leetcode.com/problems/fair-candy-swap/
 
 class Solution:
-    def fairCandySwap(self, A: List[int], B: List[int]) -> List[int]:
+    def fairCandySwap(A, B):
         diff = (sum(A) - sum(B)) // 2
         A = list(map(lambda a: a - diff, A))
         b = (list(set(A) & set(B)))[0]
