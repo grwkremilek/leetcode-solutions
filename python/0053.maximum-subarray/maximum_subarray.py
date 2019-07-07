0ad9cb800c5d80946bff9727a8b2325b14b24c01
grw.kremilek <zuzana.krenkova@gmail.com>
2019-07-07 23:52:00 +0300

    list indices

---
+++ python/0053.maximum-subarray/maximum_subarray.py
@@ -0,0 +1,7 @@
+#https://leetcode.com/problems/maximum-subarray/
+
+def maxSubArray(nums):
+    for i in range(1, len(nums)):
+        if nums[i-1] > 0:
+            nums[i] += nums[i-1]
+    return max(nums)
