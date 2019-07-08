3131ef60f682073433837ad53fe0b274fb0af801
grw.kremilek <zuzana.krenkova@gmail.com>
2019-07-08 15:10:07 +0300

    binary tree

---
+++ python/0606.construct-string-from-binary-tree/construct_string_from_binary_tree.py
@@ -0,0 +1,20 @@
+#https://leetcode.com/problems/construct-string-from-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+def tree2str(t):
+    if t:
+        res = ""
+        left = self.tree2str(t.left)
+        right = self.tree2str(t.right)
+        if left or right:
+            res += "(%s)" % left
+        if right:
+            res += "(%s)" % right
+        return str(t.val) + res
+    return ""
