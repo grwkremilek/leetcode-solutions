f3c81a0a49ce762d12a61b71fd72d2e3f29688bc
grw.kremilek <zuzana.krenkova@gmail.com>
2019-07-11 18:11:07 +0300

    (odd, even)[a % 2 == 0].append(a), sort(key = lambda)

--- python/0905.sort-array-by-parity/sort_array_parity.py
+++ python/0905.sort-array-by-parity/sort_array_parity.py
@@ -1,4 +1,4 @@
-#brute force
+
 def sortArrayByParity(A):
     evens = []
     odds = []
@@ -10,7 +10,18 @@ def sortArrayByParity(A):
     return evens + odds
 
 
-#very slow solution
+def sortArrayByParity(A):
+    odd, even = [], []
+    for a in A:
+        (odd, even)[a % 2 == 0].append(a) 
+    return even+odd
+
+
+def sortArrayByParity(A):
+    A.sort(key = lambda x: x % 2)
+    return A
+
+
 def sortArrayByParity(A):
     sorted_A = []
     for a in A:
@@ -19,3 +30,13 @@ def sortArrayByParity(A):
         else:
             sorted_A.append(a)
     return sorted_A
+    
+
+def sortArrayByParity(A):
+    i, j = 0, len(A) - 1
+    while i < j:
+        if A[i] % 2 > A[j] % 2:
+            A[i], A[j] = A[j], A[i]
+        if A[i] % 2 == 0: i += 1
+        if A[j] % 2 == 1: j -= 1
+    return A
