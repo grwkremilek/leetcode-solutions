86ffafaafb4d9232eb12ff812de8d0ba0bc10774
grw.kremilek <zuzana.krenkova@gmail.com>
2019-07-11 19:17:06 +0300

    string iteration, find character in string

---
+++ cpp/0771_jewels_and_stones.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        
+        int count = 0;
+        
+        for(auto&& s: S)
+        {
+            if (J.find(s) != std::string::npos) { ++count; }   
+        }
+        return count;
+    }
+};
