cbef0b189e9c7b84d39b03958b3a585e4d2a585c
grw.kremilek <zuzana.krenkova@gmail.com>
2019-07-11 20:43:42 +0300

    BST, recursion

--- python/0700.search-in-a-BST/README.md
+++ python/0700.search-in-a-BST/README.md
@@ -3,4 +3,5 @@
 Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
 
 
-#### Runtime: 100 ms (18.05%)
+#### Runtime: 100 ms, faster than 18.05%
+#### Runtime: 80 ms, faster than 84.09%
--- python/0700.search-in-a-BST/search_in_BST.py
+++ python/0700.search-in-a-BST/search_in_BST.py
@@ -1,8 +1,24 @@
 #https://leetcode.com/problems/search-in-a-binary-search-tree/
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 def searchBST(root, val):
         if root is None or root.val == val: 
             return root
         if root.val < val: 
-            return self.searchBST(root.right, val)
-        return self.searchBST(root.left, val)
+            return searchBST(root.right, val)
+        return searchBST(root.left, val)
+
+
+def searchBST(root, val):
+    if root and root.val == val:
+        return root
+    if root and root.val > val:
+        return searchBST(root.left, val)
+    if root and root.val < val:
+        return searchBST(root.right, val)
