6cb0617d817ebdf5adb568a8039c1e7ef026ebd6
grw.kremilek <zuzana.krenkova@gmail.com>
2019-07-16 18:55:51 +0300

    hash table with frequencies, unordered map  iteration

---
+++ cpp/0409_longest_palindrome.cpp
@@ -0,0 +1,16 @@
+#https://leetcode.com/problems/longest-palindrome/
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        
+        //create a hash table with frequencies
+        unordered_map<char, int> freqs;
+        for(char& c : s) { freqs[c]++; }
+        
+        //iterate values in the hash table
+        int counts = 0;
+        for(auto kv : freqs) { if (kv.second & 1) { counts++; } }
+        return s.size() - counts + (counts >= 1);
+    }
+};
