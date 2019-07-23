b7b28f3a6f4178c517e0ae1398bbce43a6f2096e
grw.kremilek <zuzana.krenkova@gmail.com>
2019-07-23 21:43:58 +0300

    binary tree, recursion

--- python/0617.merge-two-binary-trees/README.md
+++ python/0617.merge-two-binary-trees/README.md
@@ -5,4 +5,15 @@ Given two binary trees and imagine that when you put one of them to cover the ot
 You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
 
 
-#### Runtime: 100 ms (30.42%)
+python
+
+#### Runtime: 100 ms, faster than 30.42%
+#### Runtime: 84 ms, faster than 80.21%
+#### Runtime: 84 ms, faster than 80.21%
+
+c++
+
+#### Runtime: 36 ms, faster than 88.69%
+
+
+[leetcode solution](https://leetcode.com/problems/merge-two-binary-trees/solution/)
--- python/0617.merge-two-binary-trees/merge_two_binary_trees.py
+++ python/0617.merge-two-binary-trees/merge_two_binary_trees.py
@@ -1,8 +1,35 @@
 #https://leetcode.com/problems/merge-two-binary-trees/
 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
 def mergeTrees(t1, t2):
     if t1 or t2:
         root = TreeNode((t1 and t1.val or 0) + (t2 and t2.val or 0))
         root.left = self.mergeTrees(t1 and t1.left, t2 and t2.left)
         root.right = self.mergeTrees(t1 and t1.right, t2 and t2.right)
         return root
+
+
+def mergeTrees(t1, t2):
+    if t1 and t2:
+        root = TreeNode(t1.val + t2.val)
+        root.right = self.mergeTrees(t1.right, t2.right)
+        root.left = self.mergeTrees(t1.left, t2.left)
+        return root
+    return t1 or t2
+
+
+def mergeTrees(t1, t2):
+    if not t1: return t2;
+    if not t2: return t1;
+    t1.val += t2.val;
+    t1.left = self.mergeTrees(t1.left, t2.left);
+    t1.right = self.mergeTrees(t1.right, t2.right);
+    return t1;
