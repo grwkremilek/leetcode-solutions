7659f20d74283aa06345bd638f9dfd5645e19b5c
grw.kremilek <zuzana.krenkova@gmail.com>
2019-07-23 22:14:44 +0300

    binary tree, recursion

---
+++ cpp/0101_symmetric_tree.cpp
@@ -0,0 +1,31 @@
+#https://leetcode.com/problems/symmetric-tree/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    bool isSymmetric(TreeNode *root) {
+        
+        if (!root) { return true; }
+        return isMirror(root->left, root->right);
+    }
+    
+    bool isMirror(TreeNode* p, TreeNode* q) {
+        
+        if (!p && !q) { return true; } 
+        else if (!p || !q) { return false; }
+        
+        if (p->val != q->val) { return false; }
+        
+        return isMirror(p->left,q->right) && isMirror(p->right, q->left); 
+    }
+};
