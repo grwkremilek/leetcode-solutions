30928b6918ae02fbe12ac20533040b545bcb16e5
grw.kremilek <zuzana.krenkova@gmail.com>
2019-07-23 18:29:50 +0300

    mapping

---
+++ python/0066.plus-one/plus_one.py
@@ -0,0 +1,24 @@
+#https://leetcode.com/problems/plus-one/
+
+
+def plusOne(digits):
+    integer = ''.join(map(str, digits))
+    return list(map(int, str(int(integer) +1)))
+
+
+def plusOne(digits):
+    digits.reverse()
+    remainder = True
+    for i in range(len(digits)):
+        if remainder:
+            digits[i] += 1
+            remainder = False
+        if digits[i] > 9:
+            digits[i] = 0
+            remainder = True
+        else:
+            break
+    if remainder:
+        digits.append(1)
+    digits.reverse()
+    return digits
