50f68bf31c82ed62f3108fd3417a1182d27a1ac1
grw.kremilek <zuzana.krenkova@gmail.com>
2019-07-24 14:39:53 +0300

    cmp(), bitwise

--- python/0007.reverse-integer/README.md
+++ python/0007.reverse-integer/README.md
@@ -2,4 +2,12 @@
 
 Given a 32-bit signed integer, reverse digits of an integer.
 
+python
+
 #### Runtime: 36 ms, faster than 21.04%
+#### Runtime: 32 ms, faster than 93.47%
+
+
+c++
+
+#### Runtime: 4 ms, faster than 75.59%
--- python/0007.reverse-integer/reverse_integer.py
+++ python/0007.reverse-integer/reverse_integer.py
@@ -4,4 +4,9 @@ def reverse(x):
     sign = cmp(x, 0)
     out = int(str(sign*x)[::-1])
     return (out < 1<<31)*out*sign
-		
+
+
+def reverse(x):
+    sign = [-1,1][x>0]
+    ans = sign * int(str(abs(x))[::-1])   
+    return ans if -2**31<ans<2**31-1 else 0
