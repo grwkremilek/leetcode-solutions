f955f8283f1538f2df7c5bdc8db9ba36728589b8
grw.kremilek <zuzana.krenkova@gmail.com>
2019-08-25 13:37:51 +0200

    linked list iteration, node deletion

---
+++ cpp/0083_remove_duplicates_from_sorted_list.cpp
@@ -0,0 +1,25 @@
+https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+        ListNode* temp = head;
+        
+        while (temp and temp->next)
+        {
+            if (temp->val == temp->next->val) { temp->next = temp->next->next; }
+            else { temp = temp->next; }       
+        }
+        return head;  
+    }
+};
