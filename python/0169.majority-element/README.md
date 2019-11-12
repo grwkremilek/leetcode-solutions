05f17df9f98aa43bfb8c161cba2b4f545f77b398
grw.kremilek <zuzana.krenkova@gmail.com>
2019-11-12 20:47:56 +0100

    operator.itemgetter, divide and conquer

--- python/0169.majority-element/README.md
+++ python/0169.majority-element/README.md
@@ -4,7 +4,18 @@ Given an array of size n, find the majority element. The majority element is the
 
 You may assume that the array is non-empty and the majority element always exist in the array.
 
-#https://leetcode.com/problems/valid-sudoku/discuss/15460/1-7-lines-Python-4-solutions
+
+[python solutions with discussion on leetcode](https://leetcode.com/problems/valid-sudoku/discuss/15460/1-7-lines-Python-4-solutions)
+
+
+python
 
 #### Runtime: 56 ms, faster than 52.78%
+#### Runtime: 176 ms, faster than 98.18%
+#### Runtime: 172 ms, faster than 99.11%
+#### Runtime: 300 ms, faster than 5.35%
 
+
+c++
+
+#### Runtime: 28 ms, faster than 24.74%
--- python/0169.majority-element/majority_element.py
+++ python/0169.majority-element/majority_element.py
@@ -5,3 +5,37 @@ def majorityElement(snums):
     for n in nums:
         d[n] = d.get(n, 0) + 1
     return max(d.items(), key=operator.itemgetter(1))[0]
+
+
+def majorityElement(nums):
+        counts = collections.Counter(nums)
+        return max(counts.keys(), key=counts.get)
+        
+        
+def majorityElement(nums):
+        nums.sort()
+        return nums[len(nums)//2]
+        
+        
+def majorityElement(nums):
+    def majority_element_rec(lo, hi):
+        # base case; the only element in an array of size 1 is the majority element.
+        if lo == hi:
+            return nums[lo]
+
+        # recurse on left and right halves of this slice.
+        mid = (hi-lo)//2 + lo
+        left = majority_element_rec(lo, mid)
+        right = majority_element_rec(mid+1, hi)
+
+        # if the two halves agree on the majority element, return it.
+        if left == right:
+            return left
+
+        # otherwise, count each element and return the "winner".
+        left_count = sum(1 for i in range(lo, hi+1) if nums[i] == left)
+        right_count = sum(1 for i in range(lo, hi+1) if nums[i] == right)
+
+        return left if left_count > right_count else right
+
+    return majority_element_rec(0, len(nums)-1)
