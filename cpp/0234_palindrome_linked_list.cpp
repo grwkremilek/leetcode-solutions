25bc459bc29832e999475936ed5c0f8cc4ed2bf7
grw.kremilek <zuzana.krenkova@gmail.com>
2020-02-13 15:22:40 +0100

    vector iteration, linked list iteration

---
+++ cpp/0234_palindrome_linked_list.cpp
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/palindrome-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        
+        vector<int> numbers;
+        while(head)
+        {
+            numbers.push_back(head->val);
+            head = head->next;
+        }
+        
+         for(int i = 0, j = numbers.size() - 1; i < numbers.size(), j >= 0; i++, j--)
+         {
+             if(numbers[i] != numbers[j]) { return false; }
+         }
+        
+        return true;
+        
+    }
+};
