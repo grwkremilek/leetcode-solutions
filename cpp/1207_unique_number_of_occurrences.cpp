31ff839af65569a1a4ceae9225805621c12b7505
grw.kremilek <zuzana.krenkova@gmail.com>
2020-02-13 21:50:47 +0100

    unordered set, unordered map

---
+++ cpp/1207_unique_number_of_occurrences.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/unique-number-of-occurrences/
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        
+        unordered_map<int, int> counts;
+        unordered_set<int> s;
+  
+        for (auto a : arr)      { ++counts[a]; }
+        for (auto& p : counts)  { s.insert(p.second); }
+        return counts.size() == s.size();
+    }
+};
